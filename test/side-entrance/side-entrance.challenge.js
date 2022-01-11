11ac2f4ea59b75eae22714425925616cc48067cc
mayocca <5626046+mayocca@users.noreply.github.com>
2022-01-11 20:24:51 -0300

    ✔️ Side entrance

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -24,7 +24,36 @@ describe('[Challenge] Side entrance', function () {
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */
+        /*
+         * Explanation:
+         * 
+         * This pool keeps track of the ether deposited by each account via the
+         * `deposit` function. Its intended use is to allow each account to
+         * withdraw no more than the original amount of ether they deposited.
+         * 
+         * A malicious actor can borrow all the ether in the pool, and then
+         * deposit it back into the same pool, which in turn successfully
+         * passes the last check, the one which would revert the transaction if
+         * the ether was not returned. Even though the initial balance in the
+         * pool is the same, the sum of the balances "deposited" by each
+         * account is greater than the actual pool balance, with the attacker
+         * being able to withdraw the total amount of ether that was borrowed
+         * and deposited again in the pool. 
+         * 
+         * A contract can be made to exploit this in two transactions. First
+         * one to deploy the contract, and the second one to execute the actual
+         * exploit. Since the contract functions cannot be called externally
+         * until after the initial deployment, a "helper" contract can be made
+         * which runs the actual exploit. The initial contract deploys the
+         * helper contract, and in the same transaction, executes the flash
+         * loan against the helper contract.
+         * 
+         * This solution employed the second method to deploy and execute the
+         * exploit contract(s) in only one transaction.
+         * 
+         */
+        const ExploitFactory = await ethers.getContractFactory('SideEntranceExploit', attacker);
+        await ExploitFactory.deploy(this.pool.address);
     });
 
     after(async function () {
